0d2bd4bccc8dafc2fde96482dc60855b0f221f08
Miguel Callejón <miguel@javautodidacta.es>
2018-02-04 17:27:14 +0100

    Revert "Pequeña modificación."
    This reverts commit de1d7be

--- src/es/javautodidacta/Main.java
+++ src/es/javautodidacta/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Primer programa");
-        System.out.println("Commit and Push - Primer cambio");
     }
 }
