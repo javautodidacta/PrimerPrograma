55e82888835ae79f71e78bfe11f46ad631f5cf59
Miguel Callejón <miguel@javautodidacta.es>
2018-02-08 15:56:07 +0100

    Pequeña modificación en Main().

--- src/es/javautodidacta/Main.java
+++ src/es/javautodidacta/Main.java
@@ -3,6 +3,6 @@ package es.javautodidacta;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Primer programa");
+        System.out.println("README creado en el servidor");
     }
 }
